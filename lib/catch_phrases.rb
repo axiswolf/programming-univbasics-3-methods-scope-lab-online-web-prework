036fdb47866c6583ff25611db0dab32b040b0b1f
Karen V <karenvo@usc.edu>
2019-12-26 11:06:06 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -9,3 +9,15 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def toadstool
+  puts "Thank You Mario! But Our Princess Is In Another Castle!"
+end
+
+def link
+  puts "It's a Dangerous To Go Alone! Take This."
+end
+
+def any_phrase(argument)
+  puts "It's-a me, #{argument}!"
+end
